18c4bc340225ca7f0e0ecca0ea3f2deb3b6cba93
Roger Manrique <rogermanrique@gmail.com>
2020-04-14 10:37:35 -0300

    Alterada a mensagem de boas vindas

--- mensagem.py
+++ mensagem.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-boasvindas = u'Feja bem findo ao nofo grufo. Eu fou Fauro, o bot fanho. E fem é focê?\n\nTradução: **Seja bem vindo ao nosso grupo. Eu sou Fauro, o bot fanho. E quem é você?.**\n\nLeia o Código de Conduta e divirta-se. Apresente-se, puxe um papo, aqui somos todos amigos. Caso sinta-se ofendido por outro membro do grupo, por favor, converse com um admin.\n\nhttps://github.com/adrianlemos/code-of-conduct/blob/master/README.md \n\nSe encontrar um bug no bot de boas vindas, por favor, avise o @rogermanrique. \n\n\U0001F916'
+boasvindas = u'Feja bem findo ao nofo grufo. Eu fou Fauro, o bot fanho. E fem é focê?\n\nTradução: **Seja bem vindo ao nosso grupo. Eu sou Fauro, o bot fanho. E quem é você?.**\n\n\n\nTemos 2 regas e um código de conduta\n\n1ª Configure uma foto ou imagem de perfil.\n2ª Configure um nome de usuário.\n3ª Leia o código de conduta em https://github.com/adrianlemos/code-of-conduct/blob/master/README.md.\n\nO não cumprimento de uma das 3 regras é passível de expulsão do grupo.\n\nO seguimento destas regras serve para facilitar a interação e convivência dia membros.'
 
 userbot = u'O novo participante é um bot e só pode haver um bot no coração de vocês. Espero que esse bot seja eu.'
 
